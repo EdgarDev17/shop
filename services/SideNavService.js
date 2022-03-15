eeacbbfe2625351d0eca1654e4e65759eb0b3702
EdR50 <narutogr7@gmail.com>
2022-03-15 15:36:57 -0600

    I added a new function named getCategoriesByParent, as its name says this function handle to return a category based on user input

--- services/SideNavService.js
+++ services/SideNavService.js
@@ -2,10 +2,17 @@ import commerce from '../lib/commerce'
 
 export const getCategoriesList = () => {
     return commerce.categories
-        .list()
-        .then((res) => {
-            return res.data
-        })
-        .catch((err) => console.log(err))
+    .list()
+    .then((res) => {
+            // console.log('getCategoriesList()');
+            // console.log(res.data);
+			return res.data
+		})
+		.catch((err) => console.log(err))
 }
 
+
+export function getCategoriesByParent(slug) {
+    return commerce.categories.retrieve(slug, {type: 'slug'})
+    .then(res => console.log(res))
+}
