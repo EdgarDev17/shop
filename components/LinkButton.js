4cdab034823091cfa41a665a850f4323b1a0a652
EdR50 <narutogr7@gmail.com>
2022-03-15 15:30:26 -0600

    I remove the vertical margin class from this linkButton component, now I have to add vertical margin class outside this component, for example in a div parent

--- components/LinkButton.js
+++ components/LinkButton.js
@@ -1,10 +1,12 @@
 import Link from 'next/link'
 
 const LinkButton = ({label, pageLink, onMouseHandler}) => {
+
+
     return (
         <>
             <Link href={pageLink}>
-                <a className={'bg-black text-white text-center px-4 py-2 rounded my-5 mx-2'} onMouseEnter={onMouseHandler}>{label}</a>
+                <a className={'bg-black text-white text-center px-4 py-2 rounded mx-2'} onMouseEnter={onMouseHandler}>{label}</a>
             </Link>
         </>
     )
